fe0bbdf483087d686150316225163d921a662649
Subrath <gittest@abc.com>
2019-11-27 18:15:22 +0000

    commiting final changes in the branch

--- calculator/src/calculator/Main.java
+++ calculator/src/calculator/Main.java
@@ -21,22 +21,16 @@ public class Main {
 
 		System.out.println("Select methods: ");
 
-	/*	System.out.println("1 : +");
+		System.out.println("1 : +");
 		System.out.println("2 : /");
-<<<<<<< HEAD
-		System.out.println("3 : *");
-	*/	System.out.println("4 : -");
+  		System.out.println("3 : *");
+		System.out.println("4 : -");
 
-=======
-	*/	System.out.println("3 : *");
-	/*	System.out.println("4 : -");
-*/
->>>>>>> mul
 		int choice = sc.nextInt();
 
 		switch (choice) {
 
-	/*	case 1:
+		case 1:
 			System.out.println(cm.Addition(first, second));
 			break;
 
@@ -44,11 +38,11 @@ public class Main {
 			System.out.println(cm.Division(first, second));
 			break;
 
-	*/	case 3:
+		case 3:
 			System.out.println(cm.Multipication(first, second));
 			break;
 
-	/*	case 4:
+		case 4:
 			System.out.println(cm.Subtraction(first, second));
 			break;
 		}
